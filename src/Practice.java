06b1d141b492f1718b6707f850ad9fabe3ada4c5
Tejovathi <tejut.naidu@gmail.com>
2022-06-06 23:43:26 +0530

    This is my first program

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,10 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello Intellij");
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println("i");
+        }
+        }
     }
 }
